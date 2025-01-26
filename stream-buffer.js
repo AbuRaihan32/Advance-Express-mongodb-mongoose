4241bc629f3bd244d120898e904d0806080583b4
unknown <abumahfuz3211@gmail.com>
2025-01-27 00:17:30 +0600

    stream and buffer with row node.jg

--- stream-buffer.js
+++ stream-buffer.js
@@ -1,16 +1,34 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 //* create a server with row node.js
 const server = http.createServer();
 
 //* listen to the request event
-server.on('request', (request, res)=>{
-    console.log(request);
+server.on("request", (request, res) => {
+  if (request.url === "/read-file" && request.method === "GET") {
+    // * streaming text reading.
+    const readableStream = fs.createReadStream(
+      process.cwd() + "/texts/read.txt"
+    );
 
-    res.end('Hello form the server!')
+    readableStream.on("data", (buffer) => {
+      res.statusCode = 200;
+      res.write(buffer);
+    });
+
+    readableStream.on("end", () => {
+      res.statusCode = 200;
+      res.end("  the streaming is over");
+    });
+
+    readableStream.on("error", (err) => {
+      res.statusCode = 500;
+      res.end("something went wrong!");
+    });
+  }
 });
 
-
-server.listen(5000, ()=>{
-    console.log(`the server is running on port 5000`);
-})
+server.listen(5000, () => {
+  console.log(`the server is running on port 5000`);
+});
